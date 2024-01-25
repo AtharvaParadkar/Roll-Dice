09fa69102bb13b6e0b3834e85b98c5c1be52e40f
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-01-25 18:02:14 +0530

    CustomWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amber,
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: GradientContainerWidget(),
       ),
     ),
   ); //* runapp is a function provided by flutter framework.
+}
+
+class GradientContainerWidget extends StatelessWidget {
+  @override //* overriding the method that is expected by statelesswidget
+  Widget build(context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.red, Colors.blue],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontStyle: FontStyle.italic),
+            ),
+          ),
+        );
+  }
 }
